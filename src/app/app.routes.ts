4efe4cf6c9311b1ce94f1bb5ee9867c7cbb703aa
carlavelasco15 <carlavelasco15@gmail.com>
2023-12-08 23:46:57 +0100

    component routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { CharacterListComponent } from './character-list/character-list.component';
+import { CharacterDetailComponent } from './character-detail/character-detail.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'characters',
+        component: CharacterListComponent
+    },
+    {
+        path: 'character/:id',
+        component: CharacterDetailComponent
+    },
+    {
+        path: 'about',
+        component: AboutComponent
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    }
+];
